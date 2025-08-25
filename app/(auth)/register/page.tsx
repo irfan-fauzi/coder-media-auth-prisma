bbfe66a5649b62ba6657b5b75b1b55493df967df
irfan-fauzi <irfanwintech@yahoo.com>
2025-08-25 15:18:51 +0700

    add login & register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+
+
+const RegisterPage = () => {
+  return (
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold text-gray-500">Create acoount</h1>
+    </div>
+  )
+}
+
+export default RegisterPage
