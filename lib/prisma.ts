6884646772b7ad39086d389ff6720f1179a04999
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-09-19 18:35:26 +0700

    fix version of prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,19 @@ import { PrismaClient } from "@prisma/client"
  
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+      ],
+      
+      // Removed enableTracing as it is not a valid property
+    })
  
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
